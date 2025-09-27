dc25253c4c2fb45053d1787562a0aff6360893af
minh <minhminh20925@gmail.com>
2025-09-28 00:54:59 +0700

    tách code thành module algorithms, gui và thêm thư mục assets

--- G_8Quanxe.py
+++
@@ -1,9 +0,0 @@
-import G_8Quanxe_UI
-
-def main():
-    root, canvas, rook_img = G_8Quanxe_UI.WindowUI()
-    G_8Quanxe_UI.Draw_start_goal(canvas, rook_img)
-    root.mainloop()
-    
-if __name__ == "__main__":
-    main()
